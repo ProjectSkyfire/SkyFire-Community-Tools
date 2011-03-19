fc298c9320c9f67743dacda6e55d534f9845a3ad
AlterEgo <admin@projectskyfire.org>
2011-03-19 01:17:46 -0500

    Beginning work on new SkyFire Launcher. Not ready to use.
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

---
+++ SkyFireLauncher/SkyFireLauncher/Form2.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace SkyFirePatcher
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.InfoText = new System.Windows.Forms.Label();
+            this.StatusText = new System.Windows.Forms.Label();
+            this.ConfirmText = new System.Windows.Forms.Label();
+            this.InfoText2 = new System.Windows.Forms.Label();
+            this.Patch = new System.Windows.Forms.Button();
+            this.Wait = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // InfoText
+            // 
+            this.InfoText.Location = new System.Drawing.Point(12, 9);
+            this.InfoText.Name = "InfoText";
+            this.InfoText.Size = new System.Drawing.Size(452, 28);
+            this.InfoText.TabIndex = 0;
+            this.InfoText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // StatusText
+            // 
+            this.StatusText.Location = new System.Drawing.Point(12, 65);
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(452, 28);
+            this.StatusText.TabIndex = 1;
+            this.StatusText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ConfirmText
+            // 
+            this.ConfirmText.Location = new System.Drawing.Point(12, 93);
+            this.ConfirmText.Name = "ConfirmText";
+            this.ConfirmText.Size = new System.Drawing.Size(452, 28);
+            this.ConfirmText.TabIndex = 2;
+            this.ConfirmText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InfoText2
+            // 
+            this.InfoText2.Location = new System.Drawing.Point(12, 37);
+            this.InfoText2.Name = "InfoText2";
+            this.InfoText2.Size = new System.Drawing.Size(452, 28);
+            this.InfoText2.TabIndex = 3;
+            this.InfoText2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Patch
+            // 
+            this.Patch.Enabled = false;
+            this.Patch.Location = new System.Drawing.Point(190, 124);
+            this.Patch.Name = "Patch";
+            this.Patch.Size = new System.Drawing.Size(96, 29);
+            this.Patch.TabIndex = 4;
+            this.Patch.Text = "Launch";
+            this.Patch.UseVisualStyleBackColor = true;
+            this.Patch.Click += new System.EventHandler(this.Patch_Click);
+            // 
+            // Wait
+            // 
+            this.Wait.Interval = 1200;
+            this.Wait.Tick += new System.EventHandler(this.Wait_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 173);
+            this.Controls.Add(this.Patch);
+            this.Controls.Add(this.InfoText2);
+            this.Controls.Add(this.ConfirmText);
+            this.Controls.Add(this.StatusText);
+            this.Controls.Add(this.InfoText);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(495, 200);
+            this.MinimumSize = new System.Drawing.Size(495, 200);
+            this.Name = "Form2";
+            this.Text = "SkyFirePatcher";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InfoText;
+        private System.Windows.Forms.Label StatusText;
+        private System.Windows.Forms.Label ConfirmText;
+        private System.Windows.Forms.Label InfoText2;
+        private System.Windows.Forms.Button Patch;
+        private System.Windows.Forms.Timer Wait;
+
+
+    }
+}
---
+++ SkyFireLauncher/SkyFireLauncher/Form2.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SkyFirePatcher
+{
+    public partial class Form2 : Form
+    {
+
+        // WoW 4.0.6a 13623 (release)
+
+        byte[] unpatchedBytes = new byte[] { 0xE8, 0x42, 0xE4, 0xFF, 0xFF };
+        byte[] patchedBytes = new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00 };
+        int patchOffset = 0x91229;
+        int exeLength = 9217176;
+        byte[] wowExe = null;
+
+        bool NotConfirmed = true;
+
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                InfoText.Text = "This application will Patch Wow.exe, launch the program, then unpatch when it closes.";
+                InfoText2.Text = "Please click Patch to continue. The application will stay open in the background.";
+                StatusText.Text = "Loading Wow.exe into memory...";
+                wowExe = ReadByteArrayFromFile("wow.exe");
+                ConfirmText.Text = "Success!";
+                ConfirmText.ForeColor = Color.Green;
+            }
+            catch
+            {
+                ConfirmText.Text = "Error";
+                ConfirmText.ForeColor = Color.Red;
+            }
+
+            if (wowExe != null)
+            {
+                if (wowExe.Length != 9217176)
+                {
+                    StatusText.Text = "Wrong version, this patch will only work for WoW 4.0.6 13623.";
+                    ConfirmText.Text = "Error";
+                    ConfirmText.ForeColor = Color.Red;
+                }
+                else
+                {
+                    byte[] testPatched = new byte[unpatchedBytes.Length];
+                    System.Buffer.BlockCopy(wowExe, patchOffset, testPatched, 0, unpatchedBytes.Length);
+
+                    if (testPatched.SequenceEqual(unpatchedBytes))
+                    {
+                        StatusText.Text = "Click Launch to begin.";
+                        ConfirmText.Text = "Ready!";
+                        ConfirmText.ForeColor = Color.Orange;
+                        Patch.Text = "Launch!";
+                        Patch.Enabled = true;
+                    }
+                    if (testPatched.SequenceEqual(patchedBytes))
+                    {
+                        StatusText.Text = "Wow is already patched. Click Launch to begin.";
+                        ConfirmText.Text = "Ready!";
+                        ConfirmText.ForeColor = Color.Orange;
+                        Patch.Text = "Launch";
+                        Patch.Enabled = true;
+                    }
+                }
+            }
+        }
+
+        private void Patch_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text == "Launch!")
+            {
+                System.Buffer.BlockCopy(patchedBytes, 0, wowExe, patchOffset, patchedBytes.Length);
+                try { File.Delete("Wow_backup.exe"); } catch { }
+                try { File.Move("Wow.exe", "Wow_backup.exe"); } catch {}
+
+                if (WriteByteArrayToFile(wowExe, "Wow.exe") == true)
+                {
+                    StatusText.Text = "Successfully patched Wow.exe and created a backup Wow_backup.exe!";
+                    ConfirmText.Text = "Launching World of Warcraft";
+                    ConfirmText.ForeColor = Color.Green;
+
+                    Process WoW = new Process();
+
+                    WoW.StartInfo.FileName = "wow.exe";
+
+                    WoW.Start();
+
+                    Wait.Start();
+
+                }
+                else
+                {
+                    ConfirmText.Text = "Error!";
+                    ConfirmText.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                System.Buffer.BlockCopy(unpatchedBytes, 0, wowExe, patchOffset, unpatchedBytes.Length);
+                try { File.Delete("Wow.exe"); File.Delete("Wow_backup.exe"); } catch {};
+
+                if (WriteByteArrayToFile(wowExe, "Wow.exe") == true)
+                {
+                    StatusText.Text = "World of Warcraft is ready to play.";
+                    ConfirmText.Text = "Launching World of Warcraft";
+                    ConfirmText.ForeColor = Color.Green;
+
+                    Process WoW = new Process();
+
+                    WoW.StartInfo.FileName = "wow.exe";
+
+                    WoW.Start();
+
+                    Wait.Start();
+                    
+                }
+                else
+                {
+                    ConfirmText.Text = "Error!";
+                    ConfirmText.ForeColor = Color.Red;
+                }
+            }
+            StatusText.Text = "Waiting.";
+            Patch.Enabled = false;
+        }
+
+        public byte[] ReadByteArrayFromFile(string fileName)
+        {
+            byte[] buff = null;
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            long numBytes = new FileInfo(fileName).Length;
+            buff = br.ReadBytes((int)numBytes);
+            br.Close();
+            return buff;
+        }
+
+        public bool WriteByteArrayToFile(byte[] buff, string fileName)
+        {
+            bool response = false;
+
+            try
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite);
+                BinaryWriter bw = new BinaryWriter(fs);
+                bw.Write(buff);
+                bw.Close();
+                response = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return response;
+        }
+
+        private void IsWoWOpen()
+        {
+            if (!IsProcessOpen("Wow") && NotConfirmed)
+            {   
+                StatusText.Text = "World of Warcraft is opening";
+                
+                ConfirmText.Text = "Waiting for World of Warcraft to Open";
+                
+                ConfirmText.ForeColor = Color.Orange;
+
+                NotConfirmed = true;
+
+                IsWoWOpen();
+            }
+            else if (IsProcessOpen("Wow") && NotConfirmed)
+            {
+                StatusText.Text = "World of Warcraft is detected";
+                
+                ConfirmText.Text = "Waiting for World of Warcraft to Close";
+                
+                ConfirmText.ForeColor = Color.Green;
+
+                NotConfirmed = false;
+
+                IsWoWOpen();
+
+            }
+            else if (IsProcessOpen("Wow") && !NotConfirmed)
+            {
+                StatusText.Text = "World of Warcraft is detected";
+                
+                ConfirmText.Text = "Waiting for World of Warcraft to Close";
+                
+                ConfirmText.ForeColor = Color.Green;
+
+                IsWoWOpen();
+
+            }
+            else if (!IsProcessOpen("Wow") && !NotConfirmed)
+            {
+                StatusText.Text = "World of Warcraft has been closed";
+                
+                ConfirmText.Text = "Unpatching the client";
+                
+                ConfirmText.ForeColor = Color.Orange;
+
+                WoWIsClosed();
+            }
+
+        }
+
+        public bool IsProcessOpen(string name)
+        {
+                foreach (Process clsProcess in Process.GetProcesses())
+                {
+
+		            if (clsProcess.ProcessName.Contains(name))
+                    { 
+                        return true;
+                    }
+                }
+    
+                return false;
+
+            }
+
+        private void WoWIsClosed()
+        {
+            System.Buffer.BlockCopy(unpatchedBytes, 0, wowExe, patchOffset, unpatchedBytes.Length);
+            try { File.Delete("Wow.exe"); File.Delete("Wow_backup.exe"); }
+            catch { };
+
+            if (WriteByteArrayToFile(wowExe, "Wow.exe") == true)
+            {
+                StatusText.Text = "Successfully unpatched Wow.exe!";
+                ConfirmText.Text = "You may now close the SkyFirePatcher";
+                ConfirmText.ForeColor = Color.Green;
+
+            }
+            else
+            {
+                StatusText.Text = "Unable to unpatch Wow.exe";
+                ConfirmText.Text = "Error!";
+                ConfirmText.ForeColor = Color.Red;
+            }
+
+            Wait.Stop();
+        }
+
+        private void Wait_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Thread WoWOpen = new Thread(new ThreadStart(IsWoWOpen));
+
+                Wait.Start();
+
+                WoWOpen.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
