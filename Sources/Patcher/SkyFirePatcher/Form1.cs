0bd8ec1960b0166121baa32d2761fa1583cde5f8
Chameleon <c-n@bk.ru>
2011-06-27 01:22:02 -0400

    Patcher update

--- Sources/Patcher/SkyFirePatcher/Form1.cs
+++ Sources/Patcher/SkyFirePatcher/Form1.cs
@@ -4,66 +4,24 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.IO;
 using System.Windows.Forms;
 
 namespace SkyFirePatcher
 {
-    internal class PatchInfo
-    {
-        public const string fileWin = "Wow.exe";
-        public const string fileMac = "World of Warcraft";
-        public byte[] unpatchedBytes;
-        public byte[] patchedBytes;
-        public int patchOffset;
-        public byte[] unpatchedBytes2;
-        public byte[] patchedBytes2;
-        public int patchOffset2;
-        public int exeLength;
-        public string fileName;
-        public string backupFile;
-        internal PatchInfo(bool win)
-        {
-            
-            // WoW 4.0.6a 13623 (release)
-            if (win)
-            {
-                fileName = fileWin;
-                backupFile = "Wow_backup.exe";
-
-                exeLength = 9217176;
-
-                patchOffset = 0x91229;
-                unpatchedBytes = new byte[] { 0xE8, 0x42, 0xE4, 0xFF, 0xFF };
-                patchedBytes = new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00 };
-
-                patchOffset2 = 0x90A8D;
-                unpatchedBytes2 = new byte[] { 0x0F, 0xB5, 0x40, 0x01, 0x00, 0x00 };
-                patchedBytes2 = new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
-            }
-            else // mac
-            {
-                
-                fileName = fileMac;
-                backupFile = "World of Warcraft backup";
-
-                exeLength = 16305824;
-
-                patchOffset = 0x347E0E;
-                unpatchedBytes = new byte[] { 0xE8, 0x9D, 0xF2, 0xFF, 0xFF };
-                patchedBytes = new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00 };
-
-                patchOffset2 = 0x348BD7;
-                unpatchedBytes2 = new byte[] { 0x0F, 0x85, 0x33, 0xFE, 0xFF, 0xFF };
-                patchedBytes2 = new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90 };
-            }
-        }
-    }
     public partial class Form1 : Form
     {
         byte[] wowExe = null;
-        PatchInfo patch = null;
+        Dictionary<int, List<PatchBytes>> Patches = new Dictionary<int, List<PatchBytes>>();
+        List<PatchBytes> patches = null;
+        const string FileWin = "Wow.exe";
+        const string BackupWin = "Wow_backup.exe";
+        const string FileMac = "World of Warcraft";
+        const string BackupMac = "World of Warcraft backup";
+        string FileName;
+        string BackupFile;
 
         // WoW 4.0.6 13596 (release)
         /*byte[] unpatchedBytes = new byte[] { 0xE8, 0x42, 0xE4, 0xFF, 0xFF };
@@ -85,62 +43,75 @@ namespace SkyFirePatcher
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            foreach (Type t in Assembly.GetExecutingAssembly().GetTypes())
+            {
+                if (t.IsSubclassOf(typeof(PatchInfo)))
+                {
+                    PatchInfo pInfo = (PatchInfo)t.GetConstructor(new Type[] { }).Invoke(new Object[] { });
+                    Patches.Add(pInfo.ExeLength, pInfo.Patches);
+                }
+            }
             try
             {
                 richTextBox1.AppendText("Loading Wow.exe into memory...\n");
-                wowExe = ReadByteArrayFromFile(PatchInfo.fileWin);
+                wowExe = ReadByteArrayFromFile(FileWin);
+                if(!Patches.ContainsKey(wowExe.Length))
+                {
+                    richTextBox1.AppendText("This Wow.exe version is not supported\n");
+                    throw new Exception("Version not supported");
+                }
+                patches = Patches[wowExe.Length];
                 richTextBox1.AppendText("Success Windows 'wow.exe' loaded!\n");
-                patch = new PatchInfo(true);
+                BackupFile = BackupWin;
+                FileName = FileWin;
             }
             catch
             {
                 try
                 {
                     richTextBox1.AppendText("Loading Mac 'World of Warcraft' binary into memory...\n");
-                    wowExe = ReadByteArrayFromFile(PatchInfo.fileMac);
+                    wowExe = ReadByteArrayFromFile(FileMac);
                     richTextBox1.AppendText("Success Mac 'World of Warcraft' loaded!\n");
-                    patch = new PatchInfo(false);
+                    BackupFile = BackupMac;
+                    FileName = FileMac;
+                    if (!Patches.ContainsKey(wowExe.Length))
+                    {
+                        richTextBox1.AppendText("This Mac version is not supported\n");
+                        throw new Exception("Version not supported");
+                    }
+                    patches = patches = Patches[wowExe.Length];
                 }
                 catch
                 {
                     richTextBox1.AppendText("Could not load Win 'Wow.exe' or Mac 'World of Warcraft' into memory. Make sure this program is in your WoW directory and that WoW is closed.\n");
                     label2.Text = "Error";
                     label2.ForeColor = Color.Red;
+                    wowExe = null;
                 }
                 //filenotfound or fileinuse
             }
 
             if (wowExe != null)
             {
-                if (wowExe.Length != patch.exeLength)
-                {
-                    richTextBox1.AppendText("Wrong version, this patch will only work for WoW 4.0.6a 13623. \n");
-                    label2.Text = "Error";
-                    label2.ForeColor = Color.Red;
-                }
-                else
-                {
-                    byte[] testPatched = new byte[patch.unpatchedBytes.Length];
-                    System.Buffer.BlockCopy(wowExe, patch.patchOffset, testPatched, 0, patch.unpatchedBytes.Length);
+                byte[] testPatched = new byte[patches[0].Unpatched.Length];
+                System.Buffer.BlockCopy(wowExe, patches[0].Offset, testPatched, 0, patches[0].Unpatched.Length);
 
-                    if (testPatched.SequenceEqual(patch.unpatchedBytes))
-                    {
-                        richTextBox1.AppendText("Ready to patch " + patch.fileName + ".\n");
-                        label2.Text = "Ready!";
-                        label2.ForeColor = Color.Orange;
-                        button1.Text = "Patch";
-                        button1.Enabled = true;
-                    }
-                    if (testPatched.SequenceEqual(patch.patchedBytes))
-                    {
-                        richTextBox1.AppendText("To restore " + patch.fileName + " click \"Unpatch\".\n");
-                        label2.Text = "Ready!";
-                        label2.ForeColor = Color.Orange;
-                        button1.Text = "Unpatch";
-                        button1.Enabled = true;
-                    }
+                if (testPatched.SequenceEqual(patches[0].Unpatched))
+                {
+                    richTextBox1.AppendText("Ready to patch " + FileName + ".\n");
+                    label2.Text = "Ready!";
+                    label2.ForeColor = Color.Orange;
+                    button1.Text = "Patch";
+                    button1.Enabled = true;
                 }
-
+                if (testPatched.SequenceEqual(patches[0].Patched))
+                {
+                    richTextBox1.AppendText("To restore " + FileName + " click \"Unpatch\".\n");
+                    label2.Text = "Ready!";
+                    label2.ForeColor = Color.Orange;
+                    button1.Text = "Unpatch";
+                    button1.Enabled = true;
+                }
             }
         }
 
@@ -148,14 +119,16 @@ namespace SkyFirePatcher
         {
             if (((Button)sender).Text == "Patch")
             {
-                System.Buffer.BlockCopy(patch.patchedBytes, 0, wowExe, patch.patchOffset, patch.patchedBytes.Length);
-                System.Buffer.BlockCopy(patch.patchedBytes2, 0, wowExe, patch.patchOffset2, patch.patchedBytes2.Length);
-                try { File.Delete(patch.backupFile); } catch { }
-                try { File.Move(patch.fileName, patch.backupFile); } catch {}
-
-                if (WriteByteArrayToFile(wowExe, patch.fileName) == true)
+                foreach (PatchBytes p in patches)
                 {
-                    richTextBox1.AppendText("Successfully patched " + patch.fileName + " and created a backup " + patch.backupFile +"!\n");
+                    System.Buffer.BlockCopy(p.Patched, 0, wowExe, p.Offset, p.Patched.Length);
+                }
+                try { File.Delete(BackupFile); } catch { }
+                try { File.Move(FileName, BackupFile); } catch {}
+
+                if (WriteByteArrayToFile(wowExe, FileName) == true)
+                {
+                    richTextBox1.AppendText("Successfully patched " + FileName + " and created a backup " + BackupFile +"!\n");
                     label2.Text = "Success!";
                     label2.ForeColor = Color.Green;
                 }
@@ -167,14 +140,16 @@ namespace SkyFirePatcher
             }
             else
             {
-                System.Buffer.BlockCopy(patch.unpatchedBytes, 0, wowExe, patch.patchOffset, patch.unpatchedBytes.Length);
-                System.Buffer.BlockCopy(patch.unpatchedBytes2, 0, wowExe, patch.patchOffset2, patch.unpatchedBytes2.Length);
-                try { File.Delete(patch.fileName); File.Delete(patch.backupFile); }
+                foreach (PatchBytes p in patches)
+                {
+                    System.Buffer.BlockCopy(p.Unpatched, 0, wowExe, p.Offset, p.Unpatched.Length);
+                }
+                try { File.Delete(FileName); File.Delete(BackupFile); }
                 catch { };
 
-                if (WriteByteArrayToFile(wowExe, patch.fileName) == true)
+                if (WriteByteArrayToFile(wowExe, FileName) == true)
                 {
-                    richTextBox1.AppendText("Successfully unpatched " + patch.fileName + "!\n");
+                    richTextBox1.AppendText("Successfully unpatched " + FileName + "!\n");
                     label2.Text = "Success!";
                     label2.ForeColor = Color.Green;
                 }
---
+++ Sources/Patcher/SkyFirePatcher/Patches.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkyFirePatcher
+{
+    internal class PatchBytes
+    {
+        public int Offset;
+        public byte[] Unpatched;
+        public byte[] Patched;
+        public PatchBytes(int offset, byte[] unpatched, byte[] patched)
+        {
+            Offset = offset;
+            Unpatched = unpatched;
+            Patched = patched;
+        }
+    }
+
+    internal abstract class PatchInfo
+    {
+        public int ExeLength;
+        public List<PatchBytes> Patches = new List<PatchBytes>();
+        protected abstract void Init();
+        public PatchInfo()
+        {
+            Init();
+        }
+    }
+
+    // WoW 4.2.0 14333
+    class Patch420Win : PatchInfo
+    {
+        protected override void Init()
+        {
+            ExeLength = 10205848;
+
+            Patches.Add(new PatchBytes(0x86EFA,
+                                       new byte[] { 0x8B, 0x45, 0x0C, 0x83, 0xF8, 0x02, 0x0F, 0x85, 0xA8, 0x00, 0x00, 0x00 },
+                                       new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00, 0x90, 0xE9, 0xA9, 0x00, 0x00, 0x00, 0x90 }));
+
+            Patches.Add(new PatchBytes(0x86525,
+                                       new byte[] { 0x74, 0x28 },
+                                       new byte[] { 0xEB, 0x28 }));
+        }
+    }
+
+    // WoW 4.0.6a 13623 Windows
+    class Patch406Win : PatchInfo
+    {
+        protected override void Init()
+        {
+            ExeLength = 9217176;
+
+            Patches.Add(new PatchBytes(0x91229,
+                                       new byte[] { 0xE8, 0x42, 0xE4, 0xFF, 0xFF },
+                                       new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00 }));
+
+            Patches.Add(new PatchBytes(0x90A8D,
+                                       new byte[] { 0x0F, 0xB5, 0x40, 0x01, 0x00, 0x00 },
+                                       new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 }));
+        }
+    }
+
+    // WoW 4.0.6a 13623 Mac
+    class Patch406Mac : PatchInfo
+    {
+        protected override void Init()
+        {
+            ExeLength = 16305824;
+
+            Patches.Add(new PatchBytes(0x347E0E,
+                                       new byte[] { 0xE8, 0x9D, 0xF2, 0xFF, 0xFF },
+                                       new byte[] { 0xB8, 0x00, 0x00, 0x00, 0x00 }));
+
+            Patches.Add(new PatchBytes(0x348BD7,
+                                       new byte[] { 0x0F, 0x85, 0x33, 0xFE, 0xFF, 0xFF },
+                                       new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 }));
+        }
+    }
+}
