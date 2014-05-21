282c9a970a9a5523abcf1141ad6716b00a05db23
Sovak <Sovak007@gmail.com>
2014-05-22 00:02:58 +0200

    Updated to 548
    Signed-off-by: Sovak <Sovak007@gmail.com>

--- 5xx Patcher/Client Patcher/Patches/Windows.cs
+++ 5xx Patcher/Client Patcher/Patches/Windows.cs
@@ -22,7 +22,7 @@ namespace Connection_Patcher.Patches
         public static class x86
         {
             public static byte[] Send  = { 0x33, 0xC0, 0x90 };
-            public static byte[] Email = { 0xEB };
+            public static byte[] Email = { 0x90, 0x90, 0xEB };
             public static byte[] User  = { 0x00 };
             public static byte[] RaF   = { 0x90, 0x90, 0x90, 0x90, 0x90 };
             public static byte[] Rcv   = { 0xFF, 0x74 };
